83ae3bd72d29b6400fd844ceda591f371c922825
Logan <logan.lieou@protonmail.com>
2021-07-05 11:27:36 -0500

    added something...

---
+++ psql/README.md
@@ -0,0 +1,8 @@
+## psql
+
+We want CURD functionality for this api.
+
+C - Create
+U - Update
+R - Read
+D - Delete
--- psql/main.js
+++ psql/main.js
@@ -1,6 +1,29 @@
 // import postgres
 const { Client } = require('pg')
 
+// customize create record
+const create = async (client, table_name) => { }
+
+const update = async () => {
+	// update the client
+}
+
+const read = async (client, rows, table) => {
+	// connect to client then query
+	await client.connect()
+	const res = await client.query(`SELECT ${rows} FROM ${table}`)
+
+	// close the connection
+	await client.end()
+
+	// return the rows
+	return res.rows
+}
+
+const del = async () => {
+
+}
+
 // main function
 async function main() {	
 	// client
